93409704c4e57fd89d382167fb44201a20198f5b
Guilherme <gcellufabc@gmail.com>
2022-06-18 13:20:34 -0300

    jogo completo versao 1

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const idCard = [];
 
 for(let i=0; i<nCartas/2; i++){
 
-    let idTipo = "tipo"+i;
+    let idTipo = i;
     idCard.push(idTipo);
     idCard.push(idTipo);
     
@@ -49,9 +49,15 @@ console.log(idCard);
 idCard.sort(funcComparacao);
 console.log(idCard);
 
+const imagens = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif'];
+imagens.sort(funcComparacao);
+
 for(let i=0; i<nCartas; i++){
 
-    const templateCarta = `<div class="card ${idCard[i]}" onclick="flip(this)">${idCard[i]}<img src="./img/front.png" alt=""></div>`;
+    const templateCarta = `<div class="card">
+    <div class="back-face face tipo${idCard[i]}" onclick="flip(this)"><img src="./img/front.png"></div>
+    <div class="front-face face"><img src="./img/${imagens[idCard[i]]}"></div>
+    </div>`;
     const mesa = document.querySelector(".mesa");
         
     mesa.innerHTML += templateCarta;
@@ -64,11 +70,11 @@ function funcComparacao(){
 
 let parVirado = []
 
-function flip(elemento){ //VERIFICAR CASOS DE CLIQUE NO MESMA CARTA!!!!
+function flip(elemento){
 
     const dadosCard = elemento.classList;
     console.log("classes da carta: "+ dadosCard);
-    const tipo = dadosCard[1];
+    const tipo = dadosCard[2];
     console.log("carta: "+ tipo )
     
     if(elemento.classList.contains("flipped")){
@@ -76,19 +82,24 @@ function flip(elemento){ //VERIFICAR CASOS DE CLIQUE NO MESMA CARTA!!!!
         console.log("escolha outra!");
 
     }else{
+        
+        const back =  elemento.parentNode.querySelectorAll("div")[0];
+        const front =  elemento.parentNode.querySelectorAll("div")[1];
 
-        elemento.classList.add("flipped");
+        back.classList.add("flipped");
+        front.classList.add("flipped");
+
         parVirado.push(tipo);
         console.log(parVirado)
         
-        setTimeout(paresCard, 1000)
+        paresCard();
 
         viradas++;
     }
     
 }
 
-function paresCard(elemento){
+function paresCard(){
 
     if(parVirado.length===2){
         let card1 = document.querySelector(`.${parVirado[0]}.flipped`);
@@ -100,21 +111,25 @@ function paresCard(elemento){
             console.log("CERTO!")
             let cardsIguais=document.querySelectorAll(`.${parVirado[0]}`);
 
-            pisca(cardsIguais[0],cardsIguais[1],"acerto");
             score++;
 
             if (score === nCartas/2){
                 setTimeout(function(){
                 const body = document.querySelector("body");
                 const titulo = document.querySelector("h1");
-                pisca(body,titulo,"completo");  
+                
                 },500);
                 setTimeout(function(){
                 console.log("JOGO CONCLUIDO");
                 clearInterval(idIntervalo);
                 alert(`Você ganhou em ${viradas} jogadas!\nTempo de jogo: ${tempo} s`);
-                tempo = 0;
-                location = location;
+                const novo = prompt("Jogar novamente?");
+                console.log(novo);
+                if(novo==="sim"){
+                    location = location;
+                }else{
+                    alert("Fim de jogo.");
+                }
                 },1500);
                 
 
@@ -123,22 +138,22 @@ function paresCard(elemento){
         }else{
             console.log("ERRADO!")
             
-            pisca(card1,card2,"erro");
+            setTimeout(function() {
+                
+               
+                
+                card1.parentNode.querySelectorAll("div")[0].classList.remove("flipped");
+                card1.parentNode.querySelectorAll("div")[1].classList.remove("flipped");
 
-            setTimeout(function() {card1.classList.toggle("flipped");card2.classList.toggle("flipped");},1000);
+                card2.parentNode.querySelectorAll("div")[0].classList.remove("flipped");
+                card2.parentNode.querySelectorAll("div")[1].classList.remove("flipped");
+            
+            },2000);
             
         }
 
         parVirado=[];
     }
-}
-
-function pisca(c1,c2,escolha){
-
-    c1.classList.toggle(`${escolha}`);
-    c2.classList.toggle(`${escolha}`);
-
-    setTimeout(function() {c1.classList.toggle(`${escolha}`);c2.classList.toggle(`${escolha}`);},500);
 }
 
 function loro(){
